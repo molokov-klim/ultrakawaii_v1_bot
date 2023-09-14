c7d8563810da601641ee9e5072c2e5033bc720bd
molokov-klim <klim.molokov@mail.ru>
2023-09-15 00:01:00 +0300

    0.0.9

--- main.py
+++ main.py
@@ -278,6 +278,11 @@ async def handle_docs(message: types.Message):
     print(f"{inspect.currentframe().f_code.co_name}")
     file_id = message.document.file_id
     file_name = message.document.file_name
+
+    if '.xls' not in str(file_name) and '.xlsx' not in str(file_name):
+        await message.reply(
+            f"Документ не принят. Пожалуйста используйте формат .xls или .xlsx")
+        return
     await message.reply(
         f"Документ принят. Он будет передан нашей команде в обработку. Мы с вами свяжемся в ближайшее время")
 
@@ -297,13 +302,21 @@ async def handle_docs(message: types.Message):
         pass
 
     user_mention = f"@{username}"
+    user_id = message.from_user.id  # Получение user_id из сообщения
+
+    async with pool.acquire() as conn:
+        user_data = await get_user(conn, user_id)
 
     await bot.send_message(chat_id=config.ADMIN_ID, text=f'''
-    Принят документ от {message.from_user.first_name} {message.from_user.last_name} {user_mention} id={message.from_user.id}
+    Принят документ от {message.from_user.first_name} {message.from_user.last_name}
+    username: {user_mention}
+    email: {user_data['email']}
     ''')
     await bot.send_document(chat_id=config.ADMIN_ID, document=file_id)
     await bot.send_message(chat_id=config.ADMIN_2_ID, text=f'''
-    Принят документ от {message.from_user.first_name} {message.from_user.last_name} {user_mention} id={message.from_user.id}
+    Принят документ от {message.from_user.first_name} {message.from_user.last_name}
+    username: {user_mention}
+    email: {user_data['email']}
     ''')
     await bot.send_document(chat_id=config.ADMIN_2_ID, document=file_id)
 
